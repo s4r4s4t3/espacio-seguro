17275a8da99dcb80a92372bd2c1be9fa8a875aa2
s4r4s4t3 <sarasate8@gmail.com>
2025-06-28 07:58:49 -0300

    Cofig.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
-
-
 import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "clave_por_defecto")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
