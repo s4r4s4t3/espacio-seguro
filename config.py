ade34bbbb4712d806ce9c5649a96d98ac487e744
s4r4s4t3 <sarasate8@gmail.com>
2025-07-01 08:47:01 -0300

    Fix DATABASE_URL with replace for psycopg2

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "clave_super_secreta")
 
     # URI de la base de datos: usa PostgreSQL si está definida, sino SQLite local
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///db.sqlite3"
+    ).replace(
+        "postgres://",
+        "postgresql+psycopg2://"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
